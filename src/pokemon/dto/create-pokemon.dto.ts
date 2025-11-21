e342d25a95c22f9ac71a9fbf1c120663166a323b
Tlacaelel Leon Villaseñor <radeon777@live.com>
2025-11-21 11:15:41 -0700

    feat(create new entry on db)

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
